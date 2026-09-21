8e6de098fcfb04ebb09d32053988ac946f7745f1
Yousef0Salah <zoom28644@gmail.com>
2026-09-21 23:30:35 +0300

    finsh save aya

--- lib/modules/sura_details/quran_details_screen.dart
+++ lib/modules/sura_details/quran_details_screen.dart
@@ -1,9 +1,11 @@
+import 'package:flutter/gestures.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
 import 'package:islami/core/app_image/app_image.dart';
 import 'package:islami/core/constant/models/sura_model.dart';
 import 'package:islami/core/theme/app_colors.dart';
 import 'package:islami/core/theme/app_text.dart';
+import 'package:shared_preferences/shared_preferences.dart';
 
 class QuranDetailsScreen extends StatefulWidget {
   const QuranDetailsScreen({super.key, required this.sura});
@@ -16,6 +18,9 @@ class QuranDetailsScreen extends StatefulWidget {
 class _QuranDetailsScreenState extends State<QuranDetailsScreen> {
   List<String> suraList = [];
   bool isSplit = false;
+  String savedAya = '';
+  ScrollController? controller = ScrollController();
+  @override
   @override
   Widget build(BuildContext context) {
     if (suraList.isEmpty) {
@@ -85,20 +90,70 @@ class _QuranDetailsScreenState extends State<QuranDetailsScreen> {
                 )
               : Expanded(
                   child: SingleChildScrollView(
+                    controller: controller,
                     child: Padding(
                       padding: .all(8),
-                      child: Text.rich(
-                        textDirection: TextDirection.rtl,
-                        style: AppText.s20wB.copyWith(
-                          color: AppColors.gold,
-                          height: 3,
-                        ),
-                        TextSpan(
-                          children: suraList.map((e) {
-                            int index = suraList.indexOf(e);
-                            return TextSpan(text: '$e[${index + 1}]');
-                          }).toList(),
-                        ),
+                      child: Column(
+                        children: [
+                          if (widget.sura.id != 0 && widget.sura.id != 8)
+                            Text(
+                              'بسم الله الرحمن الرحيم',
+                              style: AppText.s20wB.copyWith(
+                                color: AppColors.gold,
+                                height: 3,
+                              ),
+                            ),
+                          Text.rich(
+                            textDirection: TextDirection.rtl,
+                            style: AppText.s20wB.copyWith(
+                              color: AppColors.gold,
+                              height: 3,
+                            ),
+                            TextSpan(
+                              children: suraList.map((e) {
+                                int index = suraList.indexOf(e);
+                                return TextSpan(
+                                  text: '$e[${index + 1}]',
+                                  style: TextStyle(
+                                    backgroundColor: e == savedAya
+                                        ? Colors.orange.withValues(alpha: 0.2)
+                                        : null,
+                                  ),
+
+                                  recognizer: TapGestureRecognizer()
+                                    ..onTap = () {
+                                      if (e == savedAya) {
+                                        _removeSaved();
+                                      } else {
+                                        saveAya(e);
+                                        savedAya = e;
+                                        setState(() {});
+                                        ScaffoldMessenger.of(
+                                          context,
+                                        ).showSnackBar(
+                                          SnackBar(
+                                            content: Center(
+                                              child: Text(
+                                                'تم حفظ الاية',
+                                                style: AppText.s20wB,
+                                              ),
+                                            ),
+                                            backgroundColor: AppColors.gold,
+                                            behavior: SnackBarBehavior.floating,
+                                            width: 200,
+                                            duration: Duration(
+                                              milliseconds: 400,
+                                            ),
+                                          ),
+                                        );
+                                      }
+                                      // getAya();
+                                    },
+                                );
+                              }).toList(),
+                            ),
+                          ),
+                        ],
                       ),
                     ),
                   ),
@@ -114,6 +169,36 @@ class _QuranDetailsScreenState extends State<QuranDetailsScreen> {
     );
     data = data.trim();
     suraList = data.split('\n');
+    getAya();
+    setState(() {});
+  }
+
+  void saveAya(String aya) async {
+    final SharedPreferences prefs = await SharedPreferences.getInstance();
+    prefs.setString('saveAya', aya);
+
+    prefs.setDouble('ayaWhere', controller!.offset);
+  }
+
+  void getAya() async {
+    final SharedPreferences prefs = await SharedPreferences.getInstance();
+    savedAya = prefs.getString('saveAya') ?? '';
+    double offset = prefs.getDouble('ayaWhere') ?? 0;
+    if (savedAya.isNotEmpty && suraList.contains(savedAya)) {
+      controller?.animateTo(
+        offset,
+        duration: Duration(seconds: 1),
+        curve: Curves.easeIn,
+      );
+    }
+    setState(() {});
+  }
+
+  void _removeSaved() async {
+    final SharedPreferences prefs = await SharedPreferences.getInstance();
+    prefs.remove('saveAya');
+    prefs.remove('ayaWhere');
+    savedAya = '';
     setState(() {});
   }
 }
